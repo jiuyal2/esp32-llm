75f3bf85c39614fc53d976765ca9418409efce90
Jiuyang Lyu <ljy030617@gmail.com>
2026-04-26 21:52:41 -0700

    undo optimization

--- main/llm.h
+++ main/llm.h
@@ -3,20 +3,13 @@
 
 /**
  * Original author of this:
- * https://github.com/karpathy/llama2.c 
- * 
+ * https://github.com/karpathy/llama2.c
+ *
  * Slight modifications added to make it ESP32 friendly
  */
 
 #include <stdio.h>
 #include <stdlib.h>
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "freertos/semphr.h"
-#include "freertos/task.h"
-#include "freertos/event_groups.h"
-
-typedef float v4sf __attribute__((aligned(16)));
 
 typedef struct {
     float prob;
@@ -38,7 +31,7 @@ typedef struct {
 
 typedef struct {
     char** vocab;
-    v4sf* vocab_scores;
+    float* vocab_scores;
     TokenIndex *sorted_vocab;
     int vocab_size;
     unsigned int max_token_length;
@@ -57,40 +50,40 @@ typedef struct {
 
 typedef struct {
     // token embedding table
-    v4sf* token_embedding_table;    // (vocab_size, dim)
+    float* token_embedding_table;    // (vocab_size, dim)
     // weights for rmsnorms
-    v4sf* rms_att_weight; // (layer, dim) rmsnorm weights
-    v4sf* rms_ffn_weight; // (layer, dim)
+    float* rms_att_weight; // (layer, dim) rmsnorm weights
+    float* rms_ffn_weight; // (layer, dim)
     // weights for matmuls. note dim == n_heads * head_size
-    v4sf* wq; // (layer, dim, n_heads * head_size)
-    v4sf* wk; // (layer, dim, n_kv_heads * head_size)
-    v4sf* wv; // (layer, dim, n_kv_heads * head_size)
-    v4sf* wo; // (layer, n_heads * head_size, dim)
+    float* wq; // (layer, dim, n_heads * head_size)
+    float* wk; // (layer, dim, n_kv_heads * head_size)
+    float* wv; // (layer, dim, n_kv_heads * head_size)
+    float* wo; // (layer, n_heads * head_size, dim)
     // weights for ffn
-    v4sf* w1; // (layer, hidden_dim, dim)
-    v4sf* w2; // (layer, dim, hidden_dim)
-    v4sf* w3; // (layer, hidden_dim, dim)
+    float* w1; // (layer, hidden_dim, dim)
+    float* w2; // (layer, dim, hidden_dim)
+    float* w3; // (layer, hidden_dim, dim)
     // final rmsnorm
-    v4sf* rms_final_weight; // (dim,)
+    float* rms_final_weight; // (dim,)
     // (optional) classifier weights for the logits, on the last layer
-    v4sf* wcls;
+    float* wcls;
 } TransformerWeights;
 
 typedef struct {
     // current wave of activations
-    v4sf *x; // activation at current time stamp (dim,)
-    v4sf *xb; // same, but inside a residual branch (dim,)
-    v4sf *xb2; // an additional buffer just for convenience (dim,)
-    v4sf *hb; // buffer for hidden dimension in the ffn (hidden_dim,)
-    v4sf *hb2; // buffer for hidden dimension in the ffn (hidden_dim,)
-    v4sf *q; // query (dim,)
-    v4sf *k; // key (dim,)
-    v4sf *v; // value (dim,)
-    v4sf *att; // buffer for scores/attention values (n_heads, seq_len)
-    v4sf *logits; // output logits
+    float *x; // activation at current time stamp (dim,)
+    float *xb; // same, but inside a residual branch (dim,)
+    float *xb2; // an additional buffer just for convenience (dim,)
+    float *hb; // buffer for hidden dimension in the ffn (hidden_dim,)
+    float *hb2; // buffer for hidden dimension in the ffn (hidden_dim,)
+    float *q; // query (dim,)
+    float *k; // key (dim,)
+    float *v; // value (dim,)
+    float *att; // buffer for scores/attention values (n_heads, seq_len)
+    float *logits; // output logits
     // kv cache
-    v4sf* key_cache;   // (layer, seq_len, dim)
-    v4sf* value_cache; // (layer, seq_len, dim)
+    float* key_cache;   // (layer, seq_len, dim)
+    float* value_cache; // (layer, seq_len, dim)
 } RunState;
 
 
@@ -100,7 +93,7 @@ typedef struct {
     RunState state; // buffers for the "wave" of activations in the forward pass
     // some more state needed to properly clean up the memory mapping (sigh)
     int fd; // file descriptor for memory mapping
-    v4sf* data; // memory mapped data pointer
+    float* data; // memory mapped data pointer
     size_t file_size; // size of the checkpoint file in bytes
 } Transformer;
 
